c0404844d86cbbba4d3c2e4b2a4a95b7f0742524
RomainStevenne <romain-ste@hotmail.com>
2019-02-22 10:57:48 +0100

    add the add_connection_mutation in the Node_Agent class

--- neat.py
+++ neat.py
@@ -20,7 +20,8 @@ class Neat_Agent:
         # a liste who contain all the connection between 2 nodes
         self.connection_genome = []
 
-        self.innovation_dic = innovation_dic
+        # init the node_genome
+        self._init_nodes()
 
     def _init_nodes(self):
         # init the nodes 
@@ -36,6 +37,39 @@ class Neat_Agent:
         # add the bias node if er is 
         if self.is_bias:
             self.node_genome.append(Node(len(self.node_genome), "bias"))
+
+    def add_connection_mutation(self, prob):
+        # add a new connection depending of the probability
+        # prob = a number between 0 and 1 who give the probability of gettin a new connection
+
+        if rd.random() <= prob:
+            # choose 2 nodes
+            inp = rd.choice(self.node_genome)
+            out = rd.choice(self.node_genome)
+
+            # check if the input is different of the output
+            if inp == out:
+                return None
+
+            # check if the output node is not a bias or an input node
+            if out.type == "bias" or out.type == "input":
+                return None
+
+            # check if er is one connection between inp and out allready exist
+            for connection in self.connection_genome:
+                if connection.input_node_id == inp.id and connection.output_node_id == out.id:
+                    return None
+
+            # check if the connnection have an innovation number else creat one
+            if (inp.id, out.id) in self.innovation_dic:
+                innov = self.innovation_dic[(inp.id, out.id)]
+
+            else:
+                innov = len(self.innovation_dic)
+                self.innovation_dic[(inp.id, out.id)] = innov
+
+            # creat the connection and append it in the genome
+            self.connection_genome.append(Connection(inp.id, out.id, innov))
 
 # a node class
 class Node:
